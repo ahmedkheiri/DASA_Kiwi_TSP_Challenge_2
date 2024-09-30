bc62506b7c43c7d26ed815fcae997cfcae2f851f
adasilva33 <108492934+adasilva33@users.noreply.github.com>
2024-09-30 19:28:12 +0100

    Update Main.py

--- Coding/Main.py
+++ Coding/Main.py
@@ -1,7 +1,4 @@
 from MCTS import MCTS
-from Node import Node
-from Data_Preprocessing import data_preprocessing
-
 
 expansion_policies = ["top_k", "ratio_k"]
 simulation_policies = [
@@ -10,22 +7,23 @@ simulation_policies = [
     "tolerance_policy",
 ]
 selection_policy = ["UCB", "UCB1T"]
+cp = [0, 1.41, 2 * 1.41]
+ratio_expansion = [0, 0.3, 0.5, 0.7, 1]
 
-
-instance_number = 4
-root_dir = "Flight connections dataset" - save instances in this folder
-instances = f"insert_your_path_to_root_dir/{root_dir}"
-instance_path = f"{instances}/{instance_number}.in"
+instance_number = 1
+root_dir = "Instances"
+instance_path = f"path_to_root/{root_dir}/{instance_number}.in"
 
 
 mcts = MCTS(
     instance=instance_path,
     instance_number=instance_number,
     number_childrens=10,
-    desired_expansion_policy="ratio_k",
-    ratio_expansion=0,
-    desired_simulation_policy="random_policy",
+    desired_expansion_policy=expansion_policies[1],
+    ratio_expansion=ratio_expansion[0],
+    desired_simulation_policy=simulation_policies[0],
     number_simulation=1,
-    desired_selection_policy="UCB",
-    cp=0,
+    desired_selection_policy=selection_policy[0],
+    cp=cp[1],
 )
+
