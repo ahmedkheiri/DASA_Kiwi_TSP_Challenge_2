724c1458b9c1c239c2c5cb3f3d152226570a9cde
adasilva33 <108492934+adasilva33@users.noreply.github.com>
2024-09-30 19:28:50 +0100

    Update Main_parallelisation.py

--- Coding/Main_parallelisation.py
+++ Coding/Main_parallelisation.py
@@ -1,37 +1,28 @@
 from MCTS_test_parralelisation import MCTS
-from Node import Node
-from Data_Preprocessing import data_preprocessing
-from Logs_process import logs_analysis
+
+expansion_policies = ["top_k", "ratio_k"]
+simulation_policies = [
+    "greedy_policy",
+    "random_policy",
+    "tolerance_policy",
+]
+selection_policy = ["UCB", "UCB1T"]
+cp = [0, 1.41, 2 * 1.41]
+ratio_expansion = [0, 0.3, 0.5, 0.7, 1]
+
+instance_number = 1
+root_dir = "Instances"
+instance_path = f"path_to_root/{root_dir}/{instance_number}.in"
 
 
-def run_mcts():
-    expansion_policies = ["top_k", "ratio_k"]
-    simulation_policies = [
-        "greedy_policy",
-        "random_policy",
-        "tolerance_policy",
-    ]
-    selection_policy = ["UCB", "UCB1T", "SP", "Bayesian"]
-
-    instance_number = 4
-    root_dir = "Flight connections dataset"
-    instances = f"/Users/adslv/Documents/LU/Term 3/Kiwi_TSP_Challenge/Code/{root_dir}"
-    instance_path = f"{instances}/{instance_number}.in"
-
-    mcts = MCTS(
-        instance=instance_path,
-        instance_number=instance_number,
-        number_childrens=10,
-        desired_expansion_policy="ratio_k",
-        ratio_expansion=0,
-        desired_simulation_policy="random_policy",
-        number_simulation=1,
-        desired_selection_policy="UCB",
-        cp=0,
-    )
-
-    # logs_analysis(root_dir=instances, create_or_not=True, do_we_delete=False)
-
-
-if __name__ == "__main__":
-    run_mcts()
+mcts = MCTS(
+    instance=instance_path,
+    instance_number=instance_number,
+    number_childrens=10,
+    desired_expansion_policy=expansion_policies[1],
+    ratio_expansion=ratio_expansion[0],
+    desired_simulation_policy=simulation_policies[0],
+    number_simulation=1,
+    desired_selection_policy=selection_policy[0],
+    cp=cp[1],
+)
